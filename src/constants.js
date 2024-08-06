a62e585bdf30a1c442077ae0c456c9678123e6ed
dagofonseca <daafonsecara@unal.edu.co>
2024-08-05 23:43:27 -0500

    Messages updated

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,13 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-      I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-      Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-    I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-    You'll learn how I built this portfolio you're currently playing through!`,
-    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-    Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-    Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-    library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-    I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-    exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
-  };
+  pc: `On my PC, I work mostly with JavaScript and TypeScript. Most of my experience is as a Frontend developer, but I also have knowledge in backend development and databases. Here is my <a href="https://github.com/dagofonseca" target="_blank">Github</a>!`,
+  "cs-degree": `I have a degree in Systems Engineering (very similar to Software Engineering) from the National University of Colombia, which provided a general understanding of the fields involved in software development.`,
+  "sofa-table": `That's my sofa. I like to relax here, watch YouTube, and listen to podcasts.`,
+  tv: `That's my TV. I recommend watching these software development YouTube channels: <a href="https://www.youtube.com/@GentlemanProgramming " target="_blank"> Gentleman Programming </a> and <a href="https://www.youtube.com/@midulive " target="_blank"> midulive </a>`,
+  bed: `This is where I sleep. It is the perfect place to recover my energy after an exhausting workday or after hitting the road on my bike (cycling is one of my hobbies).`,
+  resume: `Here is my resume <a href=" https://github.com/dagofonseca/resume/blob/main/DagoFonsecaCVEspa%C3%B1ol.pdf" target="_blank">Check it out</a>. you can contact me at dagofonseca6@gmail.com if you have any interesting job opportunities!`,
+  projects: `I’ve worked on several projects in different sectors, from financial to civil engineering. Thanks to that, I have experienced various stages and scales in software development. I also enjoy testing new ways of doing things, like this portfolio. I want to give recognition to the author jslegenddev. Check out his YouTube channel <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `To be honest, I'm not a good reader, but I'm working on becoming a better one. I have read some books about technology, such as "Clean Code" by Robert C. Martin, but most of my technology-related reading comes from blogs.`,
+  exit: `If you want to exit, just close the tab.`,
+};
